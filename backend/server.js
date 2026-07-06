897e85599938b2d2f6995fea6258e2f5946ec54b
Apple <apple@Apples-MacBook-Air.local>
2026-07-06 12:31:04 +0530

    Setup MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+// MongoDB Connection
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("🚀 Triangle Sports Backend Running");
+});
+
+// Server
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
