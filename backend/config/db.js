b49861c862ff433a4d2fe0aa9acc9083f4ee4601
Dev Verma <dev25601@gmail.com>
2026-07-14 14:00:30 +0530

    Debug Mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,10 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
+    console.log("Connected DB:");
+    console.log("Database:", mongoose.connection.name);
+    console.log("Host:", mongoose.connection.host);
+
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB Connection Failed");
