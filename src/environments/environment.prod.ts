e92198ef20e82d7a670b47ef501dfdebde4c72cb
Dev Verma <dev25601@gmail.com>
2026-09-17 01:56:10 +0530

    razorpay confirm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://triangle-store-api.onrender.com/api'
+  apiUrl: 'https://triangle-store-api.onrender.com/api',
+
+
+  razorpayKeyId: 'rzp_live_TcpDvX8NjVHmTt'
 };
